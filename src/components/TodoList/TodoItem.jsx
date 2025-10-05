9af19f31ba601221e0e4eb36bef41ed143cb7094
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-05 17:07:24 +0200

    feat: enhance TodoItem component with save functionality and improved input handling

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -1,11 +1,50 @@
 import { useState } from "react";
 import { useTodoContext } from "/src/contexts/TodoContext";
 import "./TodoItem.css";
-import { Pencil, Trash } from "lucide-react";
+import { Pencil, Save, Trash } from "lucide-react";
+
 export function TodoItem({ todo }) {
   const { actions } = useTodoContext();
   const [text, setText] = useState(todo.text);
 
+  function handleKeyDown(event) {
+    if (event.key === "Escape") {
+      setText(todo.text);
+      actions.cancelEdit({ ...todo });
+      return;
+    }
+    if (text === todo.text && event.key === "Enter") {
+      actions.cancelEdit({ ...todo });
+      return;
+    }
+    if (event.key === "Enter" && text.trim() !== "") {
+      actions.updateTodo({
+        ...todo,
+        text: text,
+        isEditing: false,
+      });
+    }
+  }
+
+  function handleChange(event) {
+    event.preventDefault();
+    setText(event.target.value);
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    console.log("handleSave called");
+    if (text.trim() === "") return;
+    if (text === todo.text) {
+      actions.cancelEdit({ ...todo });
+      return;
+    }
+    actions.updateTodo({
+      ...todo,
+      text: text,
+      isEditing: false,
+    });
+  }
   return (
     <div className="todo-item">
       <input
@@ -16,35 +55,30 @@ export function TodoItem({ todo }) {
       />
       {todo.isEditing ? (
         <input
+          autoFocus
+          onBlur={handleSave}
           defaultValue={todo.text}
-          onChange={(event) => {
-            event.preventDefault();
-            setText(event.target.value);
-          }}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
           className="todo-edit-input"
           type="text"
         />
       ) : (
-        <span className="todo-text">{todo.text}</span>
+        <span
+          onDoubleClick={() => actions.startEdit({ ...todo })}
+          className="todo-text"
+        >
+          {todo.text}
+        </span>
       )}
 
       {todo.isEditing ? (
-        <button
-          onClick={() =>
-            actions.updateTodo({
-              ...todo,
-              text: text,
-              isEditing: false,
-            })
-          }
-        >
-          Confirm
+        // This button doesn't need an onClick because the save is handled by onBlur and Enter key
+        <button className="icon">
+          <Save size={16} />
         </button>
       ) : (
-        <button
-          onDoubleClick={() => actions.startEdit({ ...todo })}
-          className="icon"
-        >
+        <button onClick={() => actions.startEdit({ ...todo })} className="icon">
           <Pencil size={16} />
         </button>
       )}
