4871690b3cec3b403791b0dc039a1d944723529b
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-06 10:24:27 +0200

    refactor: remove BoxProvider component as it is no longer needed

--- src/components/BoxProvider.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { TodoContext } from "/src/contexts/TodoContext";
-
-export function BoxProvider() {
-  const { todoState } = useContext(TodoContext);
-
-  return <h3>{todoState.todos[0].text}</h3>;
-}
