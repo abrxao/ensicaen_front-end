ab2f9712b79cf1baf024de84d57bf941bb5bf86b
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-08 11:38:09 +0200

    fix: adjust h1 font size and update navigation link styles for consistency

--- src/components/NavigationMenu/index.jsx
+++ src/components/NavigationMenu/index.jsx
@@ -1,15 +1,26 @@
 import { NavLink } from "react-router";
 import { PATHS } from "/src/paths";
 import { CheckCircle, HomeIcon } from "lucide-react";
+import { useLocation } from "react-router";
 
 export default function NavigationMenu({ children, ...props }) {
+  const location = useLocation();
+
   return (
     <nav className="navigation-menu" {...props}>
-      <NavLink to={PATHS.HOME.href} className="nav-link">
+      <NavLink
+        to={PATHS.HOME.href}
+        data-active={PATHS.HOME.href == location.pathname}
+        className="nav-link"
+      >
         <HomeIcon />
         Home
       </NavLink>
-      <NavLink to={PATHS.TODOS.href} className="nav-link">
+      <NavLink
+        to={PATHS.TODOS.href}
+        data-active={PATHS.TODOS.href == location.pathname}
+        className="nav-link"
+      >
         <CheckCircle />
         To-Do List
       </NavLink>
